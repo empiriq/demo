634ef100be7254bb727fed031d5d657c070af634
Mikhail Gerasimov <migerasimoff@gmail.com>
2025-09-26 20:36:10 +0500

    style: TerminalApplication

--- src/TerminalApplication.php
+++ src/TerminalApplication.php
@@ -4,24 +4,19 @@ namespace App;
 
 //use Symfony\Component\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Command\CompleteCommand;
 use Symfony\Component\Console\Command\HelpCommand;
 use Symfony\Component\Console\Command\ListCommand;
-use Symfony\Component\Console\Command\CompleteCommand;
-use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 class TerminalApplication extends Application
 {
     public function __construct(
-         KernelInterface $kernel,
-//        CommandLoaderInterface $commandLoader
-    )
-    {
-//        parent::__construct('Empiriq Terminal', '1.0.0');
+        KernelInterface $kernel,
+    ) {
         parent::__construct($kernel);
         $this->setAutoExit(false);
-//        $this->setCommandLoader($commandLoader);
     }
 
     protected function getDefaultCommands(): array
